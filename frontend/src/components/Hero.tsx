722684e7fa5737479b69bfaf4aa48f63dcb1c712
flameztech01 <flameztech01@gmail.com>
2026-02-19 19:52:10 +0100

    Hero Margin

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -118,7 +118,7 @@ const Hero = ({ onBookSession }: HeroProps) => {
             </div> */}
 
             {/* Headline */}
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-gray-900 mb-4 leading-tight">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-gray-900 mb-4 leading-tight mt-16">
               Is Your <span className="relative z-10 bg-gradient-to-r from-blue-700 to-blue-500 bg-clip-text text-transparent">
                   Brand
                 </span> <br className="hidden sm:block" />
